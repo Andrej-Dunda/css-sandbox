1e7acc20bdc92d2bd89ff66a33cca33050c2fb57
Andrej-Dunda <dundaandrej@seznam.cz>
2024-04-26 15:25:59 +0200

    implemented electron configuration

--- frontend/src/components/navigation/Navigation.tsx
+++ frontend/src/components/navigation/Navigation.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from 'react-router-dom';
 import './Navigation.scss'
 
 const Navigation = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="navigation">
       <h1 className='navigation-heading'>CSS Sandbox</h1>
+      <button className="home-button" onClick={() => navigate('/')}>Home</button>
     </div>
   )
 }
